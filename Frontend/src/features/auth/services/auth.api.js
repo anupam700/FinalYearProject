73692ea4c35c87d62a9f0a837aa7a681cb3aea2f
Anupam Singh <singh0anupam@gmail.com>
2026-08-25 15:32:34 +0530

    final deloyment

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://finalyearproject-owns.onrender.com",
     withCredentials: true
 })
 
